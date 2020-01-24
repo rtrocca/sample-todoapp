250334ea467482eae75a66d2cb9f10f8a8cfa771
Riccardo Trocca <rtrocca@gmail.com>
2020-01-24 19:48:14 +0100

    templates handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'react-app-polyfill/stable';
-
+// use shimming
+// https://gregbabiars.com/migrating-gulp-to-webpack/
 window.jQuery = window.$ = require('jquery');
 import 'popper.js'
 import 'bootstrap'
@@ -63,11 +64,30 @@ app.config([
         $urlRouterProvider.otherwise('/list');
     }
 ]);
+
+app.run([
+    '$compile',
+    '$templateCache',
+    function($compile, $templateCache) {
+        function requireAll(requireContext) {
+            return requireContext.keys().map(function(val){
+                return {
+                    // tpl will hold the value of your html string because thanks to wepbpack "raw-loader" **important**
+                    tpl:requireContext(val),
+    
+                    //name is just the filename
+                    name : val.split('/').pop()
+                }
+            });
+        }
+
+        let modules = requireAll(require.context("./templates/", true, /\.ng\.html$/));
+    
+        modules.map(function (val) {
+            $compile(val.tpl);
+            $templateCache.put(val.name, val.tpl);
+        })
+    }
+]);
 require('./services/services' );
 require('./components/components');
-
-
-
-
-
-
